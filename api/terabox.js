4f2dc2a1c5fa7c522c552640d1aa73bf22f51c29
Labani Behera 🎯 <158763270+Mrlabani@users.noreply.github.com>
2025-06-13 12:19:30 +0530

    Update terabox.js

--- api/terabox.js
+++ api/terabox.js
@@ -10,12 +10,16 @@ export default function handler(req, res) {
     return;
   }
 
-  // Encode the URL to base64
-  const base64 = Buffer.from(url).toString("base64");
+  try {
+    // Encode the URL to base64
+    const base64 = Buffer.from(url).toString("base64");
 
-  // Construct the final API URL
-  const bypassbotURL = `https://dl.bypassbot.workers.dev/${base64}`;
+    // Construct the final API URL
+    const bypassbotURL = `https://dl.bypassbot.workers.dev/${base64}`;
 
-  res.status(200).json({ bypassbotURL, credit: "Dev @Labani" });
+    res.status(200).json({ bypassbotURL, credit: "Dev @Labani" });
+  } catch (error) {
+    res.status(500).json({ error: "Server Error.", message: error.toString() });
+  }
 }
 
